bb0806885539eaffbff7a0d5a259279b6b4bbbcc
Fortune Domination <fortunedomination@gmail.com>
2025-11-12 01:58:39 +0000

    let the webapp be installable

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 import 'dotenv/config';
-
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
